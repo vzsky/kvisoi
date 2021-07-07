efff47b72086a11648df52b4e6a5979d8cb34b8f
Touch Sungkawichai <talay@10ninox.com>
2021-07-07 17:10:46 +0700

    change highlight on submit

--- grader_web/core/context.py
+++ grader_web/core/context.py
@@ -1,10 +1,11 @@
 from grader_web.settings import supportedLangs
+import json
 
 def supportedlangs (req) :
   lang2codemirror = {}
   for lang in supportedLangs :
     lang2codemirror[lang] = supportedLangs[lang]["codemirror"]
-  print(lang2codemirror)
   return {
-    "lang2codemirror" : lang2codemirror
+    "lang2codemirror" : lang2codemirror,
+    "json_lang2codemirror" : json.dumps(lang2codemirror),
   }
--- grader_web/core/forms.py
+++ grader_web/core/forms.py
@@ -4,5 +4,5 @@ from grader_web.settings import supportedLangs
 Langs = [(lang, supportedLangs[lang]['name']) for lang in supportedLangs]
 class SubmissionForm(forms.Form):
   taskid = forms.CharField(label='Task id', max_length=100)
-  lang = forms.CharField(widget=forms.Select(choices=Langs))
+  lang = forms.CharField(widget=forms.Select(choices=Langs, attrs={"id":"lang"}))
   code = forms.CharField(widget=forms.Textarea(attrs={"id":"editor"}))
